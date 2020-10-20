71eebb7fd6478598d30a5b8c00d07aa1d73ffe3c
Gagan <sam.gagan95@gmail.com>
2020-10-20 09:46:07 +0530

    Add files via upload

---
+++ src/app/_models/user.ts
@@ -0,0 +1,13 @@
+﻿export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    token: string;
+    role:string;
+    lastlogin:string;
+    lastlogout: string;
+    clientiplogin: string;
+    clientiplogout:string
+}
---
+++ src/app/audit/audit.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+import { UserService, AuthenticationService } from '@/_services';
+import { User } from '@/_models';
+
+@Component({ templateUrl: 'audit.component.html' })
+
+export class AuditComponent implements OnInit {
+    p: number = 1;
+    users: User[] = [];
+   
+
+    constructor(
+        private authenticationService: AuthenticationService,
+        private userService: UserService
+    ) {
+       
+    }
+
+    ngOnInit() {
+       
+        this.loadAllUsers();
+    }
+    private loadAllUsers() {
+        this.userService.getAudit().pipe(first()).subscribe(users => {
+            this.users = users;
+            console.log(users)
+        });
+    }
+
+    ngOnDestroy(): void {
+       
+      }
+}
